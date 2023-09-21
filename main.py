8a1943cf56d88cec166c7cf6f497f658bca0305f
mmogers <86738043+mmogers@users.noreply.github.com>
2023-09-21 09:08:35 +0000

    better version with rand import

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from getpass import getpass as secretInput
+import random
 print("\U0001F4A5	\U0001F4A5	\U0001F4A5 \033[1;32mGuess the Number\033[0;0m \U0001F4A5	\U0001F4A5	\U0001F4A5	")
-print("Pick the number between 0 and 1000000")
-number = int(secretInput(""))
-if number < 0 or number > 1000000:
-  print("The picked number is out of the required scope! Bye!")
-  exit()
+print("Totally Random 1000000 to 1")
 
+number = random.randint(1,1000000)
 count = 0 
 while True:
   count += 1
@@ -13,14 +10,15 @@ while True:
   print("Guess the number")
   guess = int(input(""))
   if guess < 0:
-    print("The picked number is out of the required scope! Bye!")
+    print("Your guess is out of the required scope! Bye!")
     exit()
   elif guess > number:
-    print("The number is lower")
+    print("Lower")
     continue
   elif guess < number:
-    print("The number is higher")
+    print("Higher")
     continue
   else :
     print("\n\033[32mCorrect! You guessed the number from the", count, "guess!\033[0m")
     break
+    
